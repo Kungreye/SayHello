f3d82784c2618dbd11e4fcbc799a3abb86cbc764
Kungreye <kungreye@sina.com>
2018-11-17 18:50:48 +0800

    DEBUG_TB_INTERCEPT_REDIRECTS

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,2 @@
 FLASK_APP=sayhello
 FLASK_ENV=development
-DEBUG_TB_INTERCEPT_REDIRECTS=False
--- sayhello/settings.py
+++ sayhello/settings.py
@@ -18,3 +18,5 @@ dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 SECRET_KEY = os.getenv('SECRET_KEY', 'my secret key string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
+
+DEBUG_TB_INTERCEPT_REDIRECTS = False
